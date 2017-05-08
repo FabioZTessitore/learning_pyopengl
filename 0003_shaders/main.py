50aab2dabfa539a1ad55955aa35c59365a014019
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-05-08 20:21:14 +0200

    update shaders

---
+++ 0003_shaders/main.py
@@ -0,0 +1,105 @@
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy
+import math
+
+(WIDTH, HEIGHT) = (800, 600)
+
+def key_callback(window, key, scancode, action, mode):
+    if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
+        glfw.set_window_should_close(window, GL_TRUE)
+
+def main():
+    if not glfw.init():
+        return
+
+    window = glfw.create_window(WIDTH, HEIGHT, "Hello World", None, None)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3);
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3);
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE);
+    glfw.window_hint(glfw.RESIZABLE, GL_FALSE);
+    if not window:
+        glfw.terminate()
+        return -1
+    glfw.make_context_current(window)
+
+    (width, height) = glfw.get_framebuffer_size(window)
+    glViewport(0, 0, width, height);
+
+    glfw.set_key_callback(window, key_callback);
+
+    glClearColor(0.2, 0.3, 0.2, 1.0)
+
+    triangle = [
+        -0.5, -0.5, 0.0,
+         0.5, -0.5, 0.0,
+         0.0,  0.5, 0.0
+    ]
+    triangle = numpy.array(triangle, dtype=numpy.float32)
+    indexes = [
+        0, 1, 2
+    ]
+    indexes = numpy.array(indexes, dtype=numpy.uint32)
+
+    vertex_shader_source = """
+    #version 330 core
+
+    layout (location = 0) in vec3 position;
+
+    void main()
+    {
+        gl_Position = vec4(position, 1.0f);
+    }
+    """
+
+    fragment_shader_source = """
+    #version 330 core
+
+    out vec4 color;
+    uniform vec4 ourColor;
+
+    void main()
+    {
+        color = ourColor;
+    }
+    """
+
+    vertex_shader = OpenGL.GL.shaders.compileShader(vertex_shader_source, GL_VERTEX_SHADER)
+    fragment_shader = OpenGL.GL.shaders.compileShader(fragment_shader_source, GL_FRAGMENT_SHADER)
+    shader = OpenGL.GL.shaders.compileProgram(vertex_shader, fragment_shader)
+
+    VAO = glGenVertexArrays(1)
+    VBO = glGenBuffers(1)
+    EBO = glGenBuffers(1)
+
+    glBindVertexArray(VAO)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, triangle.itemsize * len(triangle), triangle, GL_STATIC_DRAW)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexes.itemsize * len(indexes), indexes, GL_STATIC_DRAW)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+    glEnableVertexAttribArray(0)
+    glBindVertexArray(0)
+
+
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        glUseProgram(shader)
+        ourColorLoc = glGetUniformLocation(shader, "ourColor")
+        green = 0.5 * (1.0 + math.sin(glfw.get_time()))
+        glUniform4f(ourColorLoc, 0.0, green, 0.0, 1.0)
+        glBindVertexArray(VAO)
+        glDrawElements(GL_TRIANGLES, len(indexes), GL_UNSIGNED_INT, None)
+        glBindVertexArray(0)
+
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 0003_shaders/three_colors.py
@@ -0,0 +1,108 @@
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy
+
+(WIDTH, HEIGHT) = (800, 600)
+
+def key_callback(window, key, scancode, action, mode):
+    if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
+        glfw.set_window_should_close(window, GL_TRUE)
+
+def main():
+    if not glfw.init():
+        return
+
+    window = glfw.create_window(WIDTH, HEIGHT, "Hello World", None, None)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3);
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3);
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE);
+    glfw.window_hint(glfw.RESIZABLE, GL_FALSE);
+    if not window:
+        glfw.terminate()
+        return -1
+    glfw.make_context_current(window)
+
+    (width, height) = glfw.get_framebuffer_size(window)
+    glViewport(0, 0, width, height);
+
+    glfw.set_key_callback(window, key_callback);
+
+    glClearColor(0.2, 0.3, 0.2, 1.0)
+
+    triangle = [
+        # coords            # colors
+        -0.5, -0.5, 0.0,    1.0, 0.0, 0.0,
+         0.5, -0.5, 0.0,    0.0, 1.0, 0.0,
+         0.0,  0.5, 0.0,    0.0, 0.0, 1.0
+    ]
+    triangle = numpy.array(triangle, dtype=numpy.float32)
+    indexes = [
+        0, 1, 2
+    ]
+    indexes = numpy.array(indexes, dtype=numpy.uint32)
+
+    vertex_shader_source = """
+    #version 330 core
+
+    layout (location = 0) in vec3 position;
+    layout (location = 1) in vec3 color;
+
+    out vec3 ourColor;
+
+    void main()
+    {
+        gl_Position = vec4(position, 1.0f);
+        ourColor = color;
+    }
+    """
+
+    fragment_shader_source = """
+    #version 330 core
+
+    in vec3 ourColor;
+    out vec4 color;
+
+    void main()
+    {
+        color = vec4(ourColor, 1.0f);
+    }
+    """
+
+    vertex_shader = OpenGL.GL.shaders.compileShader(vertex_shader_source, GL_VERTEX_SHADER)
+    fragment_shader = OpenGL.GL.shaders.compileShader(fragment_shader_source, GL_FRAGMENT_SHADER)
+    shader = OpenGL.GL.shaders.compileProgram(vertex_shader, fragment_shader)
+
+    VAO = glGenVertexArrays(1)
+    VBO = glGenBuffers(1)
+    EBO = glGenBuffers(1)
+
+    glBindVertexArray(VAO)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, triangle.itemsize * len(triangle), triangle, GL_STATIC_DRAW)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexes.itemsize * len(indexes), indexes, GL_STATIC_DRAW)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(0)
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+    glEnableVertexAttribArray(1)
+    glBindVertexArray(0)
+
+
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        glUseProgram(shader)
+        glBindVertexArray(VAO)
+        glDrawElements(GL_TRIANGLES, len(indexes), GL_UNSIGNED_INT, None)
+        glBindVertexArray(0)
+
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
+
+
+if __name__ == '__main__':
+    main()
