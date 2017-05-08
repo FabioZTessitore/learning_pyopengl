2eff272c786642676e7403a6d7a295ce6e932b60
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-05-08 17:53:38 +0200

    updated window

---
+++ 0001_window/main.py
@@ -0,0 +1,42 @@
+import glfw
+from OpenGL.GL import *
+
+(WIDTH, HEIGHT) = (800, 600)
+
+def key_callback(window, key, scancode, action, mode):
+    if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
+        glfw.set_window_should_close(window, GL_TRUE)
+
+def main():
+    if not glfw.init():
+        return
+
+    window = glfw.create_window(WIDTH, HEIGHT, "Hello World", None, None)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3);
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3);
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE);
+    glfw.window_hint(glfw.RESIZABLE, GL_FALSE);
+    if not window:
+        glfw.terminate()
+        return -1
+    glfw.make_context_current(window)
+
+    (width, height) = glfw.get_framebuffer_size(window)
+    glViewport(0, 0, width, height);
+
+    glfw.set_key_callback(window, key_callback);
+
+    glClearColor(0.2, 0.3, 0.3, 1.0)
+
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
+
+
+if __name__ == '__main__':
+    main()
